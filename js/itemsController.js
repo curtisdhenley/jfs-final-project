b352e58849dd46aadca000388687f89cb6706890
unknown <curtisdhenley@gmail.com>
2022-06-10 14:15:55 -0400

    step 4 WIP

--- js/itemsController.js
+++ js/itemsController.js
@@ -1,13 +1,33 @@
 class ItemsController {
-    constructor(){
+    constructor(currentId=0){
         this._item = [];
+        this._currentId = currentId;
     }
 
     get item(){
         return this._item;
     }
+
+    get currentId() {
+        return this._currentId;
+    }
+
+    set item(newItem) {
+        this._item = newItem;
+    }
+
+    set currentId(newValue) {
+        this._currentId = newValue;
+    }
+
+    addItem(name, description, img, createdAt) {
+        this.currentId++
+        this.item.push([name, description, img, createdAt])
+    }
 };
 
 const bob = new ItemsController();
 
+
+bob.addItem("Apple", "tech company", "abc", "now");
 console.log(bob.item);
